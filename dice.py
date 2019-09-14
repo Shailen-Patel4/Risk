36b6688e4ca9ce3d74787a98fe0567b8969e9d71
Shailen Patel <shailen@hotmail.co.uk>
2019-09-14 19:55:28 +0100

    updated docs

--- dice.py
+++ dice.py
@@ -14,7 +14,7 @@ def rolldice(attacker, defender):
         x = np.random.randint(1,7)
         attacker_dice.append(x)
     for i in range(0,3-attacker):
-        at_dice.append(0)
+        attacker_dice.append(0)
     defender_dice.sort(reverse=True)
     attacker_dice.sort(reverse=True)
     for i in range(0, len(attacker_dice)):
