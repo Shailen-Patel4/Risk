bc8e3a84d40685e53a2bea01023b6bda142a423e
Yasmin Haque <yasminhaque97@gmail.com>
2019-10-12 16:31:23 +0100

    created a .gitignore file

--- dice.py
+++ dice.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import numpy as np
 
+
 def rolldice(attacker, defender):
     defender_dice = []
     attacker_dice = []
     determinator = []
     for i in range(0, defender):
-        x = np.random.randint(1,7)
+        x = np.random.randint(1, 7)
         defender_dice.append(x)
-    for i in range(0,3-defender):
+    for i in range(0, 3-defender):
         defender_dice.append(0)
     for i in range(0, attacker):
-        x = np.random.randint(1,7)
+        x = np.random.randint(1, 7)
         attacker_dice.append(x)
-    for i in range(0,3-attacker):
+    for i in range(0, 3-attacker):
         attacker_dice.append(0)
     defender_dice.sort(reverse=True)
     attacker_dice.sort(reverse=True)
@@ -28,8 +29,9 @@ def rolldice(attacker, defender):
             print('Defender wins')
     return attacker_dice, defender_dice, determinator
 
-[x, y, result] = rolldice(3,2)
 
-# this is a change! 
+[x, y, result] = rolldice(3, 2)
 
-print(x,y, result)
+# this is a change!
+
+print(x, y, result)
